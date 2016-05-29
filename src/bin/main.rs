ad1b57f82595e1a2eb4b9c5402d8ad0e1803ea0d
Bill Thiede <git@xinu.tv>
2016-05-28 21:30:44 -0700

    Initial draw to PNG image implemented.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,25 @@
+extern crate imagefmt;
 extern crate rustyrenderer;
 
+use imagefmt::{ColFmt, ColType};
+use rustyrenderer::draw;
+use std::path::Path;
+
 fn main() {
-    let im = rustyrenderer::draw::RGBImage {
-        w: 256,
-        h: 256,
-        buf: vec![rustyrenderer::draw::RGB{r:255,g:0,b:0}; 256*256],
-    };
-    println!("{:?}", im);
+    let (width, height) = (512, 512);
+    let mut im = draw::Image::new(width, height);
+    for x in 0..width {
+        for y in 0..height {
+            im.set(x,
+                   y,
+                   draw::RGB {
+                       r: x as u8,
+                       g: y as u8,
+                       b: (x ^ y) as u8,
+                   });
+        }
+    }
+    let out_path = Path::new("output.png");
+    println!("Saving {}", out_path.display());
+    imagefmt::write(out_path, im.w, im.h, ColFmt::RGB, &im.buf, ColType::Auto).unwrap();
 }
--- src/draw.rs
+++ src/draw.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 // TODO(wathiede): understand why this Clone is necessary to vec!
-#[derive(Clone)]
+#[derive(Copy,Clone)]
 pub struct RGB {
     pub r: u8,
     pub g: u8,
@@ -15,11 +15,33 @@ impl fmt::Debug for RGB {
 }
 
 
-pub type RGBImage = Image<RGB>;
-
 #[derive(Debug)]
-pub struct Image<Pixel> {
+pub struct Image {
     pub w: usize,
     pub h: usize,
-    pub buf: Vec<Pixel>,
+    pub buf: Vec<u8>,
+}
+
+impl Image {
+    pub fn new(w: usize, h: usize) -> Self {
+        return Image {
+            w: w,
+            h: h,
+            buf: vec![0; w*h*3],
+        };
+    }
+    pub fn set(&mut self, x: usize, y: usize, p: RGB) {
+        let off = (x + y * self.h) * 3;
+        self.buf[off + 0] = p.r;
+        self.buf[off + 1] = p.g;
+        self.buf[off + 2] = p.b;
+    }
+    // fn buffer_size(&self) -> usize {
+    // self.w * self.h * std::mem::size_of::<Pixel>()
+    // }
+    //
+
+    // pub fn get_bytes(&self) -> Vec<u8> {
+    // return self.buf.clone();
+    // }
 }
